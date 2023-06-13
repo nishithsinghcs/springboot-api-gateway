b2806d3a848729b782f64395848507abd0c97af7
nishith <nishith@interstellarsys.com>
2023-06-13 14:39:57 +0530

    commit

---
+++ employee-service-gateway/src/test/java/com/rabbitmq/consumer/EmployeeServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
